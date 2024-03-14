0add70f8c8c9b564916636eef758796d73a5bd9a
root <root@whereise>
2024-03-14 22:27:12 +0800

    add test code to app

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -4,29 +4,28 @@ class CategoriesTest < ApplicationSystemTestCase
     setup do
         @user = users(:user_one)
         @category = categories(:cat_one)
-    
         login
-      end    
+      end
 
       test "visiting the index" do
         visit categories_url
-
-        assert_selector "h1", text: "My Tasks"  
+        assert_selector "h1", text: "My Tasks"
       end
 
       test "creating a new category" do
         visit categories_url
-        
-        assert_link "Add Category", href: new_category_path
+        click_on "Add Category"
+        fill_in "Name", with: "New-Category"
+        click_on "Create Category" 
+        assert_link "New-Category"
       end
 
-      test "delete a category" do
+      test "deleting a category" do
         visit categories_url
-        
-        assert_link "Delete", href: new_category_path
+        assert_text @category.name
+        click_on "Delete", match: :first
+        assert_no_text @category.name
       end
-    
-    
 
       private
 
@@ -35,8 +34,7 @@ class CategoriesTest < ApplicationSystemTestCase
         fill_in "Email", with: @user.email
         fill_in "Password", with: "123456"
         click_on "Log in"
-
         assert_selector "h1", text: "My Tasks"
       end
-    
+
 end
--- test/system/taskapps_test.rb
+++ test/system/taskapps_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class TaskappsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
+  #  test "visiting the index" do
   #   visit taskapps_url
-  #
+  
   #   assert_selector "h1", text: "Taskapps"
   # end
 end
