ff53be7d95a262f81f88d6a07a340eac41ff5f41
root <root@whereise>
2024-03-15 21:14:37 +0800

    add code to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :categories
+  has_many :categories, dependent: :destroy
+  has_many :tasks, through: :categories
+
 end
