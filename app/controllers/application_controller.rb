f9a53c00f194ba71cd0bad28bd76e37d0143f5cd
root <root@whereise>
2024-03-15 17:26:58 +0800

    add styles to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   end
 
-  # Redirect to the categories index after sign-in
+  
   def after_sign_in_path_for(resource)
     categories_path
   end
