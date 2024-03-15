c34564bd236392d238ad87584f922511155ac811
root <root@whereise>
2024-03-15 20:37:52 +0800

    add test code to repo

--- test/system/taskapps_test.rb
+++ test/system/taskapps_test.rb
@@ -1,9 +1,34 @@
 require "application_system_test_case"
 
 class TaskappsTest < ApplicationSystemTestCase
-  #  test "visiting the index" do
-  #   visit taskapps_url
-  
-  #   assert_selector "h1", text: "Taskapps"
-  # end
+  setup do
+    @user = users(:user_one)
+    @category = categories(:cat_one)
+    login
+  end
+
+  test "creating a task" do 
+    visit category_path(@category)
+    click_on "New Task"
+    
+    fill_in "Title", with: "New Task Title"
+    fill_in "Description", with: "New Task Description"
+    select "Not Started", from: "Status"
+    fill_in "taskapp[due_date]", with: "2024-03-31"
+    click_on "Update Task"
+
+    assert_text "New Task Title"
+    assert_text "Not Started"
+
+  end
+
+  private
+
+      def login
+        visit root_path
+        fill_in "Email", with: @user.email
+        fill_in "Password", with: "123456"
+        click_on "Log in"
+        assert_selector "h1", text: "My Tasks"
+      end
 end
