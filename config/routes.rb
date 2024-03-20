a0feb0118ac90635035515a336fce718921f35cc
root <root@whereise>
2024-03-20 20:59:03 +0800

    updated codes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
-
+  root to: "devise/sessions#new"
+  
   resources :categories do
     resources :taskapps do
     end
